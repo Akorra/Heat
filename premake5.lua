4e276a8e97cf03c22448a507e64f0f2cfc89f362
Akorra <neverfearphilishere@gmail.com>
2021-09-20 22:54:43 +0100

    Added premake to generate builds

---
+++ premake5.lua
@@ -0,0 +1,104 @@
+workspace "Heat"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Heat"
+	location "Heat"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines{
+			"HT_PLATFORM_WINDOWS",
+			"HT_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+		filter "configurations:Debug"
+			defines "HZ_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "HZ_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "HZ_DIST"
+			optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Heat/vendor/spdlog/include",
+		"Heat/src"
+	}
+
+	links
+	{
+		"Heat"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines{
+			"HT_PLATFORM_WINDOWS"
+		}
+
+		filter "configurations:Debug"
+			defines "HZ_DEBUG"
+			symbols "On"
+
+		filter "configurations:Release"
+			defines "HZ_RELEASE"
+			optimize "On"
+
+		filter "configurations:Dist"
+			defines "HZ_DIST"
+			optimize "On"
